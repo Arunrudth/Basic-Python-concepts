7160b9ca61e1b3a4a19335c672d0027d4e38818e
Arunrudth <akarunkumar348@gmail.com>
2025-08-04 00:10:47 +0530

    Add files via upload

---
+++ Assignment1_Task1_ArunkumarG.py
@@ -0,0 +1,11 @@
+#Basic Mathematical Operation using Python
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+add = x+y
+sub = x-y
+mul = x*y
+divi = float(x)/float(y)
+print(f"Addition: {add}")
+print(f"Subtraction: {sub}")
+print(f"Multiplication: {mul}")
+print(f"Division: {divi}")
